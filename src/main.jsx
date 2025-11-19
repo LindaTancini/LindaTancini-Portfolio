cb9b5ddaaf664b16c9bec72fecdfddda21812c7c
LindaTancini <lindatancini95@gmail.com>
2025-11-19 12:24:48 +0100

    feat: import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 // Importazione di Bootstrap CSS e JS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// Importazione del file CSS principale
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
