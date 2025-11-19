a6f0386b24495001123a289eef59140e436b8099
LindaTancini <lindatancini95@gmail.com>
2025-11-19 12:00:46 +0100

    Scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button type="button" class="btn btn-primary">
+          Sono un bottone in app per vedere se Bootstrap funziona
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
