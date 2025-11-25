ce97343571b2b4e6e6a0cd2f088d862e74052556
LindaTancini <lindatancini95@gmail.com>
2025-11-25 11:08:25 +0100

    feat: skeleton navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,55 @@
+//Importazioni
+import { Link } from "react-router-dom";
+
+// NavBar Component
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Logo
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container d-flex align-items-center">
+        {/* LINK */}
+        <div
+          className="collapse navbar-collapse order-2 order-lg-1"
+          id="mainNav"
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+          <ul className="navbar-nav mx-auto gap-3 text-center">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
                 Home
-              </a>
+              </Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Features
-              </a>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About Me
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">
+                Progetti
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contatti
+              </Link>
             </li>
           </ul>
         </div>
+
+        {/* HAMBURGER MOBILE */}
+        <button
+          className="navbar-toggler ms-auto order-1"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+          aria-controls="mainNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
       </div>
     </nav>
   );
