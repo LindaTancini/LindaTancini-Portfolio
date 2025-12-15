318bc7ac5d734e3e54c2422ef4df2b54f829cc13
LindaTancini <lindatancini95@gmail.com>
2025-12-15 12:03:30 +0100

    feat: create array projects home

---
+++ src/data/dataProjectsHome.js
@@ -0,0 +1,30 @@
+// Array che comprende 3 progetti  da mostrare in home
+
+const homeProjects = [
+  {
+    id: 1,
+    title: "Card Blog Patisserie",
+    description:
+      "Un Design System che prende ispirazione per un ipotetico blog che pubblica ricette di dolci.",
+    image: "blog-patisserie",
+    slug: "blog-patisserie",
+  },
+  {
+    id: 2,
+    title: "Card e-commerce Gatti",
+    description:
+      "Un Design System che prende ispirazione per un ipotetico e-commerce che vende prodotti per animali, in particolare per gatti.",
+    image: "ecommerce-gatti",
+    slug: "ecommerce-gatti",
+  },
+  {
+    id: 3,
+    title: "Movies Blog",
+    description:
+      "Progetto Full Stack individuale sulla creazione e gestione di un blog di film. Realizzato con React per il front-end e Node.js con Express per il back-end.",
+    image: "movies-blog",
+    slug: "movies-blog",
+  },
+];
+
+export default homeProjects;
