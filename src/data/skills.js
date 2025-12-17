332b869e71c7c758da6dd8837deeb8c12f8edf48
LindaTancini <lindatancini95@gmail.com>
2025-12-17 12:15:32 +0100

    feat: skills array

---
+++ src/data/skills.js
@@ -0,0 +1,13 @@
+const skills = [
+  { name: "HTML", icon: "/public/icon/html.svg" },
+  { name: "CSS", icon: "/public/icon/css.svg" },
+  { name: "JavaScript", icon: "/public/icon/javascript.svg" },
+  { name: "Typescript", icon: "/public/icon/typescript.svg" },
+  { name: "React", icon: "/public/icon/react.svg" },
+  { name: "Storybook", icon: "/public/icon/storybook.svg" },
+  { name: "Node.js", icon: "/public/icon/nodejs.svg" },
+  { name: "Express", icon: "/public/icon/express.svg" },
+  { name: "MySql", icon: "/public/icon/mysql.svg" },
+];
+
+export default skills;
