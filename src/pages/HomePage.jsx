9968901e3781127216a36dcc6c76ac8af42e3483
LindaTancini <lindatancini95@gmail.com>
2025-12-17 12:20:11 +0100

    feat: import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../components/HeroSection";
 import ProjectHomePreview from "../components/ProjectHomePreview";
+import SkillsCarousel from "../components/SkillsCarousel";
 
 function HomePage() {
   return (
     <div>
       <HeroSection />
+      <SkillsCarousel />
       <ProjectHomePreview />
     </div>
   );
