7aa56a89852251648eb708383b7b0c640e499fb0
LindaTancini <lindatancini95@gmail.com>
2025-12-18 12:11:46 +0100

    feat: change src

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,13 +1,13 @@
 const skills = [
-  { name: "HTML", icon: "/public/icon/html.svg" },
-  { name: "CSS", icon: "/public/icon/css.svg" },
-  { name: "JavaScript", icon: "/public/icon/javascript.svg" },
-  { name: "Typescript", icon: "/public/icon/typescript.svg" },
-  { name: "React", icon: "/public/icon/react.svg" },
-  { name: "Storybook", icon: "/public/icon/storybook.svg" },
-  { name: "Node.js", icon: "/public/icon/nodejs.svg" },
-  { name: "Express", icon: "/public/icon/express.svg" },
-  { name: "MySql", icon: "/public/icon/mysql.svg" },
+  { name: "HTML", icon: "/icon/html.svg" },
+  { name: "CSS", icon: "/icon/css.svg" },
+  { name: "JavaScript", icon: "/icon/javascript.svg" },
+  { name: "Typescript", icon: "/icon/typescript.svg" },
+  { name: "React", icon: "/icon/react.svg" },
+  { name: "Storybook", icon: "/icon/storybook.svg" },
+  { name: "Node.js", icon: "/icon/nodejs.svg" },
+  { name: "Express", icon: "/icon/express.svg" },
+  { name: "MySql", icon: "/icon/mysql.svg" },
 ];
 
 export default skills;
