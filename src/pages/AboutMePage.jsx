a59d6889f05be26e0854ec603291997be52a5575
LindaTancini <lindatancini95@gmail.com>
2026-01-13 11:49:33 +0100

    feat: about me skeleton

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,9 +1,50 @@
+// Importazioni
+import AboutMeImg from "../assets/me.jpg";
+import AboutMeCats from "../assets/me-and-cats.jpg";
+import AboutMeManga from "../assets/me-manga-and-cats.jpg";
+import AboutMeGame from "../assets/me-game-and-cats.jpg";
+import AboutSection from "./AboutSection";
+
+// AboutMePage component
 function AboutMePage() {
   return (
-    <section>
-      <h1>About me</h1>
-      <p>Scrivere di me</p>
-    </section>
+    <>
+      {/* Sezione Me */}
+
+      <AboutSection title="Chi sono e cosa faccio" image={AboutMeImg}>
+        <p>Qua ci sarà il testo che parla di me</p>
+      </AboutSection>
+
+      {/* Sezione Gatti */}
+
+      <AboutSection
+        title="Leone e Fumetto, i miei gatti"
+        image={AboutMeCats}
+        reverse
+      >
+        <p>Qua ci sarà il testo che parla dei miei gatti</p>
+      </AboutSection>
+
+      {/* Sezione Manga&Anime */}
+
+      <AboutSection
+        title="Manga e Anime, una delle mie più grandi passioni"
+        image={AboutMeManga}
+        reverse
+      >
+        <p>Qua ci sarà il testo che parla della mia passioen per i manga</p>
+      </AboutSection>
+
+      {/* Sezione Gaming */}
+
+      <AboutSection
+        title="Videogiochi, una passione che mi accompagna da sempre"
+        image={AboutMeGame}
+        reverse
+      >
+        <p>Qua ci sarà il testo che parla della mia passione per il gaming</p>
+      </AboutSection>
+    </>
   );
 }
 
