816dd19c34084ab12bb9f76ee53745aa5ba2768a
LindaTancini <lindatancini95@gmail.com>
2026-02-18 11:52:22 +0100

    feat: projects array

---
+++ src/data/dataProjectsCard.js
@@ -0,0 +1,78 @@
+// Array che comprende i progetti da mostrare nella card dei progetti
+
+const projectsCard = [
+  {
+    id: 1,
+    title: "Card Blog Patisserie",
+    description:
+      "Un Design System che prende ispirazione per un ipotetico blog che pubblica ricette di dolci, in particolare di pasticceria home-made.",
+    image: "/public/projects/blog-patisserie.png",
+    category: "Design System",
+    slug: "blog-patisserie",
+  },
+  {
+    id: 2,
+    title: "Card e-commerce Gatti",
+    description:
+      "Un Design System che prende ispirazione per un ipotetico e-commerce che vende prodotti per animali, in particolare per gatti.",
+    image: "/public/projects/ecommerce-gatti.png",
+    category: "Design System",
+    slug: "ecommerce-gatti",
+  },
+  {
+    id: 3,
+    title: "Portfolio Personale",
+    description:
+      "Progetto indivuale del mio portfolio personale volto a vetrina digitale dei miei lavori e delle mie competenze.",
+    image: "/public/projects/portfolio-personale.png",
+    category: "Front-end",
+    slug: "portfolio-personale",
+  },
+  {
+    id: 4,
+    title: "Boolshop",
+    description:
+      "Progetto di gruppo full stack basato sulla questione di un e-commerce di cd e vinili.",
+    image: "/public/projects/boolshop.png",
+    category: "Full-stack",
+    slug: "boolshop",
+  },
+  {
+    id: 5,
+    title: "Cine Blog",
+    description:
+      "Progetto indivuale full stack basato sulla questione di un blog cinematografico.",
+    image: "/public/projects/movies-blog.png",
+    category: "Full-stack",
+    slug: "cine-blog",
+  },
+  {
+    id: 6,
+    title: "Photo Blog",
+    description:
+      "Progetto indivuale front-end, sul DOM, di una bacheca di foto.",
+    image: "/public/projects/photo-blog.png",
+    category: "Front-end",
+    slug: "photo-blog",
+  },
+  {
+    id: 7,
+    title: "DC Comics",
+    description:
+      "Progetto indivuale React basato sulla realizazzione di una pagina dedicata ai fumetti DC.",
+    image: "/public/projects/dc-comics.png",
+    category: "Front-end",
+    slug: "dc-comics",
+  },
+  {
+    id: 8,
+    title: "Spotify Clone",
+    description:
+      "Progetto indivuale basato sulla realizzazione di una pagina dedicata a Spotify.",
+    image: "/public/projects/spotify.png",
+    category: "Front-end",
+    slug: "spotify-clone",
+  },
+];
+
+export default projectsCard;
