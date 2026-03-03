208ec3bd371bfbb80033bfa35dc15f2c9b33f6f0
LindaTancini <lindatancini95@gmail.com>
2026-03-03 15:05:14 +0100

    refactor: replace hardcoded sections with dynamic mapping from aboutSections data

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,49 +1,20 @@
 // Importazioni
-import AboutMeImg from "../assets/me.jpg";
-import AboutMeCats from "../assets/me-and-cats.jpg";
-import AboutMeManga from "../assets/me-manga-and-cats.jpg";
-import AboutMeGame from "../assets/me-game-and-cats.jpg";
 import AboutSection from "./AboutSection";
+import aboutSections from "../data/dataAboutMe";
 
-// AboutMePage component
 function AboutMePage() {
   return (
     <>
-      {/* Sezione Me */}
-
-      <AboutSection title="Chi sono e cosa faccio" image={AboutMeImg}>
-        <p>Qua ci sarà il testo che parla di me</p>
-      </AboutSection>
-
-      {/* Sezione Gatti */}
-
-      <AboutSection
-        title="Leone e Fumetto, i miei gatti"
-        image={AboutMeCats}
-        reverse
-      >
-        <p>Qua ci sarà il testo che parla dei miei gatti</p>
-      </AboutSection>
-
-      {/* Sezione Manga&Anime */}
-
-      <AboutSection
-        title="Manga e Anime, una delle mie più grandi passioni"
-        image={AboutMeManga}
-        reverse
-      >
-        <p>Qua ci sarà il testo che parla della mia passioen per i manga</p>
-      </AboutSection>
-
-      {/* Sezione Gaming */}
-
-      <AboutSection
-        title="Videogiochi, una passione che mi accompagna da sempre"
-        image={AboutMeGame}
-        reverse
-      >
-        <p>Qua ci sarà il testo che parla della mia passione per il gaming</p>
-      </AboutSection>
+      {aboutSections.map((section, index) => (
+        <AboutSection
+          key={index}
+          title={section.title}
+          image={section.image}
+          reverse={index % 2 !== 0}
+        >
+          <p>{section.text}</p>
+        </AboutSection>
+      ))}
     </>
   );
 }
