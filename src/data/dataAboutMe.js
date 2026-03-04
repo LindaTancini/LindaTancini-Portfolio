bcee49123d8bd830efb95e12c0f914e2488e494b
LindaTancini <lindatancini95@gmail.com>
2026-03-04 15:04:46 +0100

    feat: add id

--- src/data/dataAboutMe.js
+++ src/data/dataAboutMe.js
@@ -2,28 +2,25 @@
 import AboutMeImg from "../assets/me.jpg";
 import AboutMeCats from "../assets/me-and-cats.jpg";
 import AboutMeManga from "../assets/me-manga-and-cats.jpg";
-import AboutMeGame from "../assets/me-game-and-cats.jpg";
 
 const aboutSections = [
   {
-    title: "Chi sono e cosa faccio",
+    id: 1,
+    subtitle: "Chi sono e cosa faccio",
     image: AboutMeImg,
     text: "Qua ci sarà il testo che parla di me",
   },
   {
-    title: "Leone e Fumetto, i miei gatti",
+    id: 2,
+    subtitle: "Leone e Fumetto, i miei gatti",
     image: AboutMeCats,
     text: "Qua ci sarà il testo che parla dei miei gatti",
   },
   {
-    title: "Manga e Anime, una delle mie più grandi passioni",
+    id: 3,
+    subtitle: "I miei hobby: Manga, Anime e Videogiochi",
     image: AboutMeManga,
-    text: "Qua ci sarà il testo che parla della mia passione per i manga",
-  },
-  {
-    title: "Videogiochi, una passione che mi accompagna da sempre",
-    image: AboutMeGame,
-    text: "Qua ci sarà il testo che parla della mia passione per il gaming",
+    text: "Qua ci sarà il testo che parla delle mie passioni",
   },
 ];
 
