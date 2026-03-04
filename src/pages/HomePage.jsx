fcbbedda884b313eb4245ffe746de63decde5866
LindaTancini <lindatancini95@gmail.com>
2026-03-04 15:56:20 +0100

    feat: remove SkillsCarousel component and associated styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import HeroSection from "../components/HeroSection";
 import ProjectHomePreview from "../components/ProjectHomePreview";
-import SkillsCarousel from "../components/SkillsCarousel";
 
 function HomePage() {
   return (
     <div>
       <HeroSection />
-      <SkillsCarousel />
       <ProjectHomePreview />
     </div>
   );
