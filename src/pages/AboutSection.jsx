9a2e394b62627c9798442092b947a85ca7ff5561
LindaTancini <lindatancini95@gmail.com>
2026-03-04 15:05:01 +0100

    feat: update AboutSection to use subtitle prop and improve layout structure

--- src/pages/AboutSection.jsx
+++ src/pages/AboutSection.jsx
@@ -1,19 +1,22 @@
-function AboutSection({ title, image, children, reverse = false }) {
+function AboutSection({ subtitle, image, children, reverse = false }) {
   return (
     <section className="py-4 py-md-5">
       <div className="container">
-        <h2 className="mb-4 text-center">{title}</h2>
-
         <div
           className={`row align-items-center ${
             reverse ? "flex-md-row-reverse" : ""
           }`}
         >
-          <div className="col-12 col-md-6 mb-3 mb-md-0">
-            <img src={image} alt={title} className="img-fluid rounded" />
+          {/* Immagine */}
+          <div className="col-12 col-md-6 mb-4 mb-md-0">
+            <img src={image} alt={subtitle} className="img-fluid rounded" />
           </div>
 
-          <div className="col-12 col-md-6">{children}</div>
+          {/*  Testo */}
+          <div className="col-12 col-md-6">
+            <h2 className="h3 mb-3 text-center text-md-start">{subtitle}</h2>
+            <div className="about-content">{children}</div>
+          </div>
         </div>
       </div>
     </section>
