b110aed2657e3cf2ffd53637d59af4133494c3c9
LindaTancini <lindatancini95@gmail.com>
2026-03-04 15:05:05 +0100

    feat: refactor AboutMePage to improve section structure and use subtitle prop

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -5,10 +5,17 @@ import aboutSections from "../data/dataAboutMe";
 function AboutMePage() {
   return (
     <>
+      {/* Titolo */}
+      <section className="py-5 text-center">
+        <div className="container">
+          <h1 className="display-4 mb-4">About Me</h1>
+        </div>
+      </section>
+
       {aboutSections.map((section, index) => (
         <AboutSection
-          key={index}
-          title={section.title}
+          key={section.id}
+          subtitle={section.subtitle}
           image={section.image}
           reverse={index % 2 !== 0}
         >
