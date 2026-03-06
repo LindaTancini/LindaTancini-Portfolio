4c60c7e763e798d89fc6b2b18acec0b86fd7d7ea
LindaTancini <lindatancini95@gmail.com>
2026-03-06 11:46:47 +0100

    feat: add project filtering functionality to ProjectCardList component

--- src/components/ProjectCardList.jsx
+++ src/components/ProjectCardList.jsx
@@ -1,12 +1,52 @@
 // Importazioni
 import ProjectCard from "./ProjectCard";
 import dataProjectsCard from "../data/dataProjectsCard";
+import { useState } from "react";
 
 function ProjectCardList() {
+  const [category, setCategory] = useState("all");
+
+  const filteredProjects =
+    category === "all"
+      ? dataProjectsCard
+      : dataProjectsCard.filter((project) => project.category === category);
+
   return (
     <div className="container">
+      {/* Filtri */}
+      <div className="project-filters mb-5 text-center">
+        <button
+          className={`btn btn-filter ${category === "all" ? "active" : ""}`}
+          onClick={() => setCategory("all")}
+        >
+          Tutti
+        </button>
+
+        <button
+          className={`btn btn-filter ${category === "Front-end" ? "active" : ""}`}
+          onClick={() => setCategory("Front-end")}
+        >
+          Front-end
+        </button>
+
+        <button
+          className={`btn btn-filter ${category === "Design System" ? "active" : ""}`}
+          onClick={() => setCategory("Design System")}
+        >
+          Design System
+        </button>
+
+        <button
+          className={`btn btn-filter ${category === "Full-stack" ? "active" : ""}`}
+          onClick={() => setCategory("Full-stack")}
+        >
+          Full-stack
+        </button>
+      </div>
+
+      {/* Card */}
       <div className="row g-4">
-        {dataProjectsCard.map((project) => (
+        {filteredProjects.map((project) => (
           <div className="col-md-4" key={project.id}>
             <ProjectCard {...project} />
           </div>
