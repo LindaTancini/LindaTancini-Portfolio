f43c117aa27d62da1b1ffe479f2f1a00ad161c7e
LindaTancini <lindatancini95@gmail.com>
2026-03-08 18:36:26 +0100

    feat: add motion and change classname

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,6 +1,7 @@
 import SkillsBlock from "../components/SkillsBlock";
 import { aboutSections, skills } from "../data/dataAboutMe";
 import AboutSection from "./AboutSection";
+import { motion } from "framer-motion";
 
 function AboutMePage() {
   return (
@@ -8,11 +9,23 @@ function AboutMePage() {
       {/* Titolo  */}
       <section className="py-5 text-center">
         <div className="container">
-          <h1 className="display-4 mb-3">Un mondo di pixel e fusa</h1>
-          <p className="about-intro">
+          <motion.h1
+            className="display-4 mb-3"
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            Un mondo di pixel e fusa
+          </motion.h1>
+          <motion.p
+            className="about-intro"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
+          >
             Linda è una sviluppatrice front-end appassionata di interfacce
             moderne, animazioni e design pulito.
-          </p>
+          </motion.p>
         </div>
       </section>
 
@@ -31,12 +44,14 @@ function AboutMePage() {
       {/* Sezione Skills */}
       <section className="py-5">
         <div className="container">
-          <h2 className="text-center mb-5">Competenze tecniche e Soft Skill</h2>
+          <h2 className="about-title text-center mb-5">
+            Competenze tecniche e Soft Skill
+          </h2>
 
           {/* Hard Skills */}
           {Object.entries(skills.hard).map(([category, skillList]) => (
             <div key={category} className="mb-4">
-              <h3 className="text-capitalize mb-3">{category}</h3>
+              <h3 className="mb-3 text-category">{category}</h3>
 
               <div className="skills-grid">
                 {skillList.map((skill, index) => (
@@ -53,7 +68,7 @@ function AboutMePage() {
 
           {/* Soft Skills */}
           <div className="mt-5">
-            <h3 className="mb-3">Soft Skills</h3>
+            <h3 className="mb-3 text-category">Soft Skills</h3>
             <div className="skills-grid">
               {skills.soft.map((skill, index) => (
                 <SkillsBlock key={index} icon="🐾" skill={skill} type="soft" />
