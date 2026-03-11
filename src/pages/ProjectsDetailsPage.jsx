ed16c0107f4a597d101c47ae99ac706994c11d48
LindaTancini <lindatancini95@gmail.com>
2026-03-11 17:46:57 +0100

    feat: add carousel and some changes

--- src/pages/ProjectsDetailsPage.jsx
+++ src/pages/ProjectsDetailsPage.jsx
@@ -62,8 +62,8 @@ function ProjectsDetailsPage() {
       {/* Immagine principale */}
 
       <div className="row project-hero align-items-start mb-5">
-        <h1 className="project-detailtitle text-center ">{project.title}</h1>
-        <p className="project-detaildescription mt-3 mb-4 text-center ">
+        <h1 className="project-detailtitle text-center">{project.title}</h1>
+        <p className="project-detaildescription mt-3 mb-4 text-center">
           {project.description}
         </p>
         <div className="col-lg-7 mb-4">
@@ -125,8 +125,7 @@ function ProjectsDetailsPage() {
 
       {project.challenges && (
         <div className="project-challenges">
-          <h3 className="mb-3">Sfide affrontate</h3>
-
+          <h3 className="project-semititle mb-3">Sfide affrontate</h3>
           <ul>
             {project.challenges.map((challenge, index) => (
               <li key={index}>{challenge}</li>
@@ -136,48 +135,95 @@ function ProjectsDetailsPage() {
       )}
 
       {/* Galleria */}
+
       {project.gallery && (
         <div className="project-gallery mt-5">
-          <h3 className="mb-4">Galleria</h3>
-          <div className="row">
-            {project.gallery.map((imgObj, index) => (
-              <div key={index} className="col-md-4 mb-4">
-                <img
-                  src={imgObj.src}
-                  alt={imgObj.title}
-                  className="img-fluid gallery-img"
-                  onClick={() => openImage(index)}
-                />
-              </div>
-            ))}
+          <h3 className="project-semititle text-center mb-4 fs-1">Galleria</h3>
+          <p className="project-detaildescription text-center mb-4">
+            Vuoi dare un’occhiata più da vicino al progetto? Ecco alcune
+            immagini!
+          </p>
+
+          <div
+            id="projectCarousel"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              {project.gallery.map((imgObj, index) => (
+                <div
+                  key={index}
+                  className={`carousel-item ${index === 0 ? "active" : ""}`}
+                >
+                  <img
+                    src={imgObj.src}
+                    className="d-block w-100 gallery-carousel-img"
+                    alt={imgObj.title}
+                    onClick={() => openImage(index)}
+                  />
+
+                  <div className="carousel-caption">
+                    <p>{imgObj.title}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* Freccia sinistra */}
+
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#projectCarousel"
+              data-bs-slide="prev"
+            >
+              <span className="carousel-control-prev-icon"></span>
+            </button>
+
+            {/* Freccia destra */}
+
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#projectCarousel"
+              data-bs-slide="next"
+            >
+              <span className="carousel-control-next-icon"></span>
+            </button>
           </div>
         </div>
       )}
 
       {/* Lightbox Overlay */}
+
       {selectedIndex !== null && (
         <div className="image-overlay" onClick={closeImage}>
           <button className="lightbox-arrow left" onClick={prevImage}>
             &#10094;
           </button>
+
           <div className="lightbox-content">
             <img
               src={project.gallery[selectedIndex].src}
               alt={project.gallery[selectedIndex].title}
               className="image-preview"
             />
+
             <p className="lightbox-title">
               {project.gallery[selectedIndex].title}
             </p>
           </div>
+
           <button className="lightbox-arrow right" onClick={nextImage}>
             &#10095;
           </button>
         </div>
       )}
+
       {/* TORNA AI PROGETTI */}
-      <div className="text-center mb-5 mt-4">
-        <Link to="/projects" className="btn btn-back ">
+
+      <div className="text-center mb-5 mt-5">
+        <Link to="/projects" className="btn btn-back">
           ← Torna ai progetti
         </Link>
       </div>
