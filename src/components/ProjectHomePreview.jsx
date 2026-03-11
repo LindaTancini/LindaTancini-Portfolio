5a488b96da8da9df280baa2bc6a9699fe63c4122
LindaTancini <lindatancini95@gmail.com>
2026-03-11 10:57:59 +0100

    feat: some changes for projects home

--- src/components/ProjectHomePreview.jsx
+++ src/components/ProjectHomePreview.jsx
@@ -1,31 +1,51 @@
-// Importazioni
 import dataProjectsHome from "../data/dataProjectsHome";
 import { Link } from "react-router-dom";
 
-// ProjectHomePreview Component
 function ProjectHomePreview() {
   return (
     <section className="projects-preview py-5">
+      <h2 className="project-home-title text-center mb-3">
+        Progetti in evidenza
+      </h2>
+      <p className="text-center project-home-intro">
+        Alcuni dei miei progetti più recenti e significativi. Clicca su "Scopri
+        di più" per vedere i dettagli di ciascun progetto 🐱
+      </p>
+
       <div className="container">
-        <h2 className="text-center mb-5">I miei progetti preferiti:</h2>
-        <div className="row g-4 justify-content-center">
+        <div className="row">
           {dataProjectsHome.map((project) => (
-            <div key={project.id} className="col-md-6 col-lg-4">
-              {/* Card progetti */}
-              <div className="project-card">
+            <div key={project.slug} className="col-md-4 mb-4">
+              <div className="project-card card h-100 border-0">
                 {/* Immagine */}
                 <img
                   src={project.image}
+                  className="card-img-top project-img"
                   alt={project.title}
-                  className="project-img"
                 />
-                {/* Titolo e Descrizione */}
-                <h5 className="mt-3">{project.title}</h5>
-                <p>{project.description}</p>
-                {/* Bottone */}
-                <Link to="/projects" className="btn btn-outline-pink mt-2">
-                  Scopri di più
-                </Link>
+
+                <div className="card-body d-flex flex-column p-4">
+                  {/* Titolo */}
+                  <h5 className="card-title project-title">{project.title}</h5>
+
+                  {/* Badge */}
+                  <span className="badge project-badge mb-2">
+                    {project.category}
+                  </span>
+
+                  {/* Descrizione */}
+                  <p className="card-text project-description">
+                    {project.description}
+                  </p>
+
+                  {/* Bottone */}
+                  <Link
+                    to={`/projects/${project.slug}`}
+                    className="btn btn-project mt-auto"
+                  >
+                    Scopri di più
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
