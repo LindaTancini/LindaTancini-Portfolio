c6e7f8eaa9b880731a70395b545acd10d057b0b6
LindaTancini <lindatancini95@gmail.com>
2026-03-11 11:51:54 +0100

    feat: add fixed navbar and scrolling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,51 @@
-//Importazioni
+// Importazioni
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-// NavBar Component
 function NavBar() {
+  // Stato per gestire l'effetto di scorrimento
+  const [scrolled, setScrolled] = useState(false);
+
+  // Effetto per rilevare lo scroll e aggiornare lo stato
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    // Aggiungi l'evento di scroll
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg ">
+    <nav
+      className={`navbar navbar-expand-lg fixed-top ${
+        scrolled ? "navbar-scrolled" : ""
+      }`}
+    >
       <div className="container-fluid">
-        {/* Logo */}
+        {/* Logo e link alla home */}
         <Link className="navbar-brand d-flex align-items-center" to="/">
           <img src={logo} alt="Logo" style={{ width: "60px" }} />
         </Link>
-        {/* LINK */}
-        <div
-          className="collapse navbar-collapse order-2 order-lg-1"
-          id="mainNav"
-        >
-          <ul className="navbar-nav mx-auto gap-5 ">
+
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav mx-auto gap-5">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
-
             <li className="nav-item">
               <Link className="nav-link" to="/about">
                 About Me
               </Link>
             </li>
-
             <li className="nav-item">
               <Link className="nav-link" to="/projects">
                 Progetti
               </Link>
             </li>
-
             <li className="nav-item">
               <Link className="nav-link" to="/contact">
                 Contatti
@@ -43,9 +54,9 @@ function NavBar() {
           </ul>
         </div>
 
-        {/* HAMBURGER MOBILE */}
+        {/* Bottone per il menu a tendina su dispositivi mobili */}
         <button
-          className="navbar-toggler ms-auto order-1"
+          className="navbar-toggler ms-auto"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#mainNav"
